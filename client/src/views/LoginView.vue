adc15af4d57e0431dc439eb94acf4d2d89ffe9f2
José Correia <javcorreia@gmail.com>
2025-03-17 23:29:43 +0000

    client updates

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,11 +1,9 @@
 <script setup>
 import {ref} from 'vue'
 import axios from 'axios'
-import { useRouter } from 'vue-router'
 
 const email = ref('')
 const password = ref('')
-const router = useRouter()
 const error = ref('')
 
 const handleSubmit = async () => {
@@ -21,7 +19,7 @@ const handleSubmit = async () => {
 
         const {token} = response.data
         sessionStorage.setItem('token', token)
-        await router.push('/')
+        window.location.href = '/'
     } catch (error) {
         console.error(error)
         error.value = error
@@ -30,38 +28,37 @@ const handleSubmit = async () => {
 </script>
 
 <template>
-    <div class="login">
-        <form @submit.prevent="handleSubmit">
-            <div>
-                <label for="email">Email:</label>
-                <input
-                    id="email"
-                    type="email"
-                    v-model="email"
-                    required
-                />
-            </div>
-            <div>
-                <label for="password">Password:</label>
-                <input
-                    id="password"
-                    type="password"
-                    v-model="password"
-                    required
-                />
-            </div>
-            <button type="submit">Login</button>
-        </form>
-        <div v-if="error" class="error">{{ error }}</div>
-    </div>
+    <section class="flex items-center justify-center min-h-screen ">
+        <div class="bg-gray-500 shadow-md rounded-lg p-8 max-w-md w-full">
+            <h1 class="text-2xl font-semibold text-center text-gray-800 mb-6">Login</h1>
+            <form @submit.prevent="handleSubmit">
+                <div>
+                    <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                    <input
+                        id="email"
+                        type="email"
+                        v-model="email"
+                        required
+                        placeholder="Enter your email"
+                        class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm text-gray-700 focus:ring-blue-500 focus:border-blue-500"
+                    />
+                </div>
+                <div>
+                    <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+                    <input
+                        id="password"
+                        type="password"
+                        v-model="password"
+                        required
+                        placeholder="Enter your password"
+                        class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm text-gray-700 focus:ring-blue-500 focus:border-blue-500"
+                    />
+                </div>
+                <div>
+                    <button type="submit" class="w-full bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">Login</button>
+                </div>
+            </form>
+            <div v-if="error" class="mt-4 text-center text-sm font-medium text-red-500">{{ error }}</div>
+        </div>
+    </section>
 </template>
-
-<style>
-  @media (min-width: 1024px) {
-    .login {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
-    }
-  }
-</style>
